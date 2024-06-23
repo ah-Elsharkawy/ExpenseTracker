aa414c951bccd2fd37900b3a36458dca8d143bc7
ahmed-fahmy17 <fahomahmed@gmail.com>
2024-06-23 03:31:45 +0300

    adding notifications endpoint

---
+++ src/ExpenseTracker.Application/IServices/INotificationAppService.cs
@@ -0,0 +1,15 @@
+﻿using ExpenseTracker.Dto;
+using ExpenseTracker.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker.IServices
+{
+    public interface INotificationAppService
+    {
+        Task<List<NotificationDTO>> GetNotifications();
+    }
+}
---
+++ src/ExpenseTracker.Application/Services/NotificationAppService.cs
@@ -0,0 +1,48 @@
+﻿using Abp.Application.Services;
+using Abp.Application.Services.Dto;
+using Abp.Domain.Repositories;
+using Abp.ObjectMapping;
+using ExpenseTracker.Dto;
+using ExpenseTracker.Enums;
+using ExpenseTracker.IServices;
+using ExpenseTracker.Models;
+using ExpenseTracker.Roles.Dto;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker.Services
+{
+    public class NotificationAppService : ApplicationService, INotificationAppService
+    {
+        private readonly IRepository<Notifications> _repository;
+        private readonly IObjectMapper _objectMapper;
+
+        public NotificationAppService(IRepository<Notifications> repository, IObjectMapper objectMapper)
+        {
+            _repository = repository;
+            _objectMapper = objectMapper;
+        }
+
+        public async Task<List<NotificationDTO>> GetNotifications()
+        {
+            try
+            {
+                var uId = AbpSession.UserId;
+                if (uId == null)
+                    return null;
+
+                var notifications = await _repository.GetAllListAsync();
+                return new List<NotificationDTO>(ObjectMapper.Map<List<NotificationDTO>>(notifications));
+            }
+            catch(Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
